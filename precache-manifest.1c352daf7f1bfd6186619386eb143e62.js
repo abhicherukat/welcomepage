0dd0f4dab55c40efce40e9a30b550046a5b8dbec
abhi <abhideepak22@gmail.com>
2020-08-10 18:27:18 +0530

    Updates

---
+++ precache-manifest.1c352daf7f1bfd6186619386eb143e62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24bd1bdb7dd183e9d54e60d0fb73a0f",
+    "url": "/welcomepage/index.html"
+  },
+  {
+    "revision": "cf88b3e0ba6deb3995ec",
+    "url": "/welcomepage/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "dbdc711126564ecd970f",
+    "url": "/welcomepage/static/css/main.8b076cef.chunk.css"
+  },
+  {
+    "revision": "cf88b3e0ba6deb3995ec",
+    "url": "/welcomepage/static/js/2.6e822bc9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/welcomepage/static/js/2.6e822bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbdc711126564ecd970f",
+    "url": "/welcomepage/static/js/main.1bc09540.chunk.js"
+  },
+  {
+    "revision": "0b7b2f742ed6e56dc0c9",
+    "url": "/welcomepage/static/js/runtime-main.43877119.js"
+  }
+]);
